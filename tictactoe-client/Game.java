e10df589e8d051160cdbcc3a484175435ff78e34
Megan Booher <meganboo@cheyenne.cs.colostate.edu>
2020-12-01 12:35:51 -0700

    added pom.xml

--- tictactoe-client/Game.java
+++
@@ -1,9 +0,0 @@
-
-public class Game {
-	public static void main(String[] args) {
-		System.out.println("----Welcome to tictactoe!----");
-		System.out.println("Get three in a row to win!");
-		
-
-	}
-}
---
+++ tictactoe-client/src/main/java/TP/Game.java
@@ -0,0 +1,11 @@
+package TP;
+import org.apache.commons.httpclient.*;
+
+public class Game {
+	public static void main(String[] args) {
+		System.out.println("----Welcome to tictactoe!----");
+		System.out.println("Get three in a row to win!");
+		
+
+	}
+}
