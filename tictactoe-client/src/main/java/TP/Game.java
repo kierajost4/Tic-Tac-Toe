b8d73aa25c0a6adc84251f8b6d65030163b91b2d
Kiera Jost <kieraj@denver.cs.colostate.edu>
2020-12-01 13:30:47 -0700

    fixed pom.xml

--- tictactoe-client/src/main/java/TP/Game.java
+++ tictactoe-client/src/main/java/TP/Game.java
@@ -1,11 +1,9 @@
 package TP;
-import org.apache.commons.httpclient.*;
+import org.apache.http.*;
 
 public class Game {
 	public static void main(String[] args) {
 		System.out.println("----Welcome to tictactoe!----");
 		System.out.println("Get three in a row to win!");
-		
-
 	}
 }
